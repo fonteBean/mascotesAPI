e5c70fa7d81839178b9a5d365ab6fa534ab1d6b2
fonteBean <n9ckbean@gmail.com>
2025-08-12 16:54:34 -0300

    Add: serve.js content

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const app = express();
+const routes = require('./routes/mascotesRouter');
+require('dotenv').config();
+const PORT = process.env.API_PORT;
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(routes);
+
+app.listen(PORT, ()=>{
+  console.log(`Servidor rodando na porta ${PORT}`);
+})
+
